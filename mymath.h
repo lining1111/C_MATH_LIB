35b719b764ba5e8540012f6e7f085dea9db4b406
李宁 <770677655@qq.com>
2018-11-01 13:22:28 +0800

    Update mymath.h

--- mymath.h
+++ mymath.h
@@ -11,5 +11,7 @@
 #define getbit(x, y) ((x) >> (y)&1)
 
 bool get_bit_and(uint32_t num1, uint32_t num2, uint8_t bit);
+bool get_array_indexof(uint8_t *org_array, uint16_t org_array_size, uint8_t *check_array, uint16_t check_array_size,
+                     uint16_t *site);
 
 #endif
